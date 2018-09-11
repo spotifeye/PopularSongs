a456d590eebfaebe1899386f72c3c48fd1bb626a
Andrew MacDonald <MAC@Andrews-MacBook-Pro-4.local>
2018-09-10 18:00:33 -0700

    API and seeding script refactored

--- database/db.js
+++
@@ -1,4 +0,0 @@
-mongoose.connect('mongodb://localhost:27017');
-
-const Artist = require('./SchemaModes');
-
--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var db = require('../database-mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const Artist = require('../database/index');
 
-var app = express();
+const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,26 +10,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use( bodyParser.json() );
 
 
-app.get('/api/users', function (req, res) {
-  db.getAllUsers((err, data) => {
-    if(err) {
-      console.log(err);
-    } else {
-      res.json(data);
-    }
-  });
+app.get('/artist/:id', function (req, res) {
+
+  let artistID = parseInt(req.params.id, 10);
+  Artist.findOne({id: artistID})
+    .then(artist => res.json(artist))
+    .catch(err => console.log(err));
+
 });
 
 
-app.post('/api/transfer', function (req, res) {
-  db.updateTransferAmts(req.body.transferingID, req.body.transferingIDAmt , req.body.receivingID, req.body.receivingIDAmt , req.body.amount, (err, data) => {
-    if(err) {
-      console.log(err);
-    } else {
-      res.json({message: 'Success'});
-    }
-  });
+// expect to receive {artistID, albumID, songID, added -> bool either 1 or 0}
+app.post('/artist/update', function (req, res) {
+  let update = {};
+  var objProp = `albums.${req.body.albumID}.songs.${req.body.songID}.library`;
+  update[objProp] = !!parseInt(req.body.added,10);
+
+  Artists.findOneAndUpdate({id: req.body.artistID}, {$set:update})
+    .then(() => res.json({message: 'success', added: !!parseInt(req.body.added,10)}))
+    .catch(() => res.status(400).json({message: 'bad request'}));
 });
+
 
 const PORT = 3003;
 

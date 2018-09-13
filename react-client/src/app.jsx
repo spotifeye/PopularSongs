c5dbae07d165657a6df41fa0949b99c48c3c6d45
Andrew MacDonald <MAC@Andrews-MacBook-Pro-4.local>
2018-09-12 21:58:57 -0700

    set up react and jest config

---
+++ react-client/src/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import $ from 'jquery';
+// import Balance from './components/Balance.jsx';
+// import List from './components/List.jsx';
+//import axios from 'axios';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    };
+}
+  
+  render () {
+   
+    return (
+        <h1>Popular Songs</h1>
+    );
+  }
+}
+
+export default App;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,26 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import $ from 'jquery';
-// import Balance from './components/Balance.jsx';
-// import List from './components/List.jsx';
-import axios from 'axios';
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-     
-    };
-}
-  
-  render () {
-   
-    return (
-        <h1>Hello React</h1>
-    );
-  }
-}
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('app'));
